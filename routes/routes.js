bebb5ca7d0fe283b155e66543acc1588cc53a40b
Prateek Mathur <prtprateekmathur@gmail.com>
2017-12-02 20:22:55 +0530

    server check!

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,7 @@ var storage = multer.diskStorage({
         cb(null, config.pdfuploadLocation)
     },
     filename: function (req, file, cb) {
+       console.log(file);
         cb(null, file.originalname + '.pdf') 
     }
 })
